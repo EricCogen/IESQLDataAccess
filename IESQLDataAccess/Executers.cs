0362d2f9bc8e86b7e17d47fb3a0338138e85c1fe
Eric Cogen <ericcogen@gmail.com>
2017-10-10 18:25:52 -0400

    Initial Commit

---
+++ IESQLDataAccess/Executers.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CCIH.Utilities.IESQLDataAccess
+{
+    public class Executers
+    {
+        public string Status { get; private set; }
+        private string _dbConn = string.Empty;
+        public int commandTimeOut = 120;
+
+        public Executers(string dbConnectionString)
+        {
+            _dbConn = dbConnectionString;
+        }
+
+        public Executers(string Server, string DataBase, string UserName, string Password)
+        {
+            _dbConn = $"Server={Server};Database={DataBase};User Id={UserName};Password={Password};";
+        }
+
+        public Executers(string Server, string DataBase, bool Trusted_Connection = true)
+        {
+            _dbConn = $"Server={Server};Database={DataBase};Trusted_Connection=True;";
+        }
+
+        private void LoadParams(ref SqlCommand cmd, Parameters Params, string proc)
+        {
+            try
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = commandTimeOut;
+                cmd.CommandText = proc;
+
+                for (int p = 0; p < Params.parameters.Count; p++)
+                {
+                    if (Params.parameters[p].maxLength < 1)
+                    {
+                        cmd.Parameters.Add(Params.parameters[p].parameterName, Params.parameters[p].dbType).Value = Params.parameters[p].parameterValue;
+                    }
+                    else
+                    {
+                        cmd.Parameters.Add(Params.parameters[p].parameterName, Params.parameters[p].dbType, Params.parameters[p].maxLength).Value = Params.parameters[p].parameterValue;
+                    }
+                }
+
+                cmd.Connection = new SqlConnection(_dbConn);
+                cmd.Connection.Open();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Execute A Stored Procedure
+        /// </summary>
+        /// <param name="nameOfStoredProcedure">Name Of The Stored Procedure To Execute</param>
+        /// <param name="Params">Parameters</param>
+        /// <example>
+        /// Parameters p = new Parameters();
+        /// p.Add("@FirstName", SqlDbType.VarChar, "Eric");
+        /// Executers exec = new Executers(dbConnectionString);
+        /// exec.Execute("dbo.MyStoredProcedure", myParams);
+        /// </example>
+        public void Execute(string nameOfStoredProcedure, Parameters Params)
+        {
+            SqlCommand cmd = null;
+            try
+            {
+                cmd = new SqlCommand();
+                LoadParams(ref cmd, Params, nameOfStoredProcedure);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Status = $"Message: {ex.Message}\r\nStackTrace: {ex.StackTrace}";
+            }
+            finally
+            {
+                cmd.Connection.Close();
+                cmd.Connection.Dispose();
+                cmd.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Execute A Stored Procedure: Fill A DataTable
+        /// </summary>
+        /// <param name="nameOfStoredProcedure">Name Of The Stored Procedure To Execute</param>
+        /// <param name="Params">Parameters</param>
+        /// <param name="DT">DataTable</param>
+        /// <example>
+        /// DataTable myDataTable = new DataTable();
+        /// Parameters p = new Parameters();
+        /// p.Add("@FirstName", SqlDbType.VarChar, "Eric");
+        /// 
+        /// Executers exec = new Executers(dbConnectionString);
+        /// exec.Execute("dbo.MyStoredProcedure", myParams, myDataTable);
+        /// 
+        /// if(!string.IsNullOrEmpty(exec.Status))
+        /// {
+        ///     //An Error Occurred: Check The exec.Status variable;
+        /// }
+        /// else if(myDataTable != null && myDataTable.Rows != null)
+        /// {
+        ///     ForEach(DataRow dr in myDataTable.Rows)
+        ///     {
+        ///         string myColumnName = dr.Field<string>("myColumnName");
+        ///     }
+        /// }
+        /// </example>
+        public void Execute(string nameOfStoredProcedure, Parameters Params, ref DataTable DT)
+        {
+            DT = Execute(Params, nameOfStoredProcedure);
+        }
+
+        /// <summary>
+        /// Execute A Stored Procedure: Returns A DataTable;
+        /// </summary>
+        /// <param name="Params">Parameters</param>
+        /// <param name="nameOfStoredProcedure">Name Of The Stored Procedure To Execute</param>
+        /// <returns>DataTable</returns>
+        public DataTable Execute(Parameters Params, string nameOfStoredProcedure)
+        {
+            DataTable DT = new DataTable();
+            SqlCommand cmd = null;
+            try
+            {
+                cmd = new SqlCommand();
+                LoadParams(ref cmd, Params, nameOfStoredProcedure);
+                DT.Load(cmd.ExecuteReader());
+            }
+            catch (Exception ex)
+            {
+                Status = $"Message: {ex.Message}\r\nStackTrace: {ex.StackTrace}";
+            }
+            finally
+            {
+                cmd.Connection.Close();
+                cmd.Connection.Dispose();
+                cmd.Dispose();
+            }
+            return DT;
+        }
+    }
+}
---
+++ IESQLDataAccess/Parameters.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CCIH.Utilities.IESQLDataAccess
+{
+    public class Parameters
+    {
+        public class Parameter
+        {
+            public string parameterName;
+            public SqlDbType dbType;
+            public object parameterValue;
+            public int maxLength = -1;//varchar(max)
+        }
+
+        public List<Parameter> parameters
+        {
+            get;
+            private set;
+        }
+
+        public Parameters()
+        {
+            parameters = new List<Parameter>();
+        }
+
+        /// <summary>
+        /// Adds An Element To The List Of Parameters
+        /// </summary>
+        /// <param name="parameterName">Name of the parameter in the stored procedure</param>
+        /// <param name="type">System.Data.SqlTypes</param>
+        /// <param name="myParameterValue">The Objects value; string/decimal/int/long/etc.</param>
+        /// <param name="maxLength">For strings; If you need to pass in the max length of the string</param>
+        /// <example>parametes.Add("@PersonID", SqlDbType.Int, 12345, 0)</example>
+        /// <example>parametes.Add("@FirstName", SqlDbType.VarChar, "Eric", 50)</example>
+        public void Add(string parameterName, SqlDbType dbType, object parameterValue, int maxLength = -2)
+        {
+            FixParameterName(ref parameterName);
+            if (maxLength > 0)
+            {
+               parameterValue = Truncate(parameterValue, maxLength);
+            }
+            parameters.Add(new Parameter() { parameterName = parameterName, dbType = dbType, parameterValue = parameterValue, maxLength = maxLength });
+        }
+
+        private string Truncate(object parameterValue, int maxLength)
+        {
+            //todo: check for dbNull value also;
+            maxLength = maxLength - 1;//account for starting index at 0;
+            string _myString = Convert.ToString(parameterValue);
+
+            if (string.IsNullOrEmpty(_myString))
+            {
+                _myString = string.Empty;
+            }
+            else
+            {
+                try
+                {
+                    if (_myString.Length > maxLength)
+                    {
+                        return _myString.Substring(0, maxLength);                        
+                    }
+                }
+                catch (Exception)
+                {
+                    return Convert.ToString(parameterValue);
+                }
+            }
+            return _myString;
+        }
+
+        public void Clear()
+        {
+            parameters.Clear();
+        }
+
+        public void Dispose()
+        {
+            Clear();
+            parameters = null;
+        }
+
+        /// <summary>
+        /// Trim, Prefix If Necessary An @ Character/Symbol;
+        /// </summary>
+        /// <param name="myParam">@MyParameter</param>
+        private void FixParameterName(ref string myParam)
+        {
+            myParam = myParam.Trim();
+            if (!myParam.StartsWith("@"))
+            {
+                myParam = "@" + myParam;
+            }
+        }
+    }
+}
